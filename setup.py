c5debc1144cca943ab6bc1cdc546e9bf8112b318
Hiroshi Toyama <toyama0919@gmail.com>
2024-02-07 15:55:36 +0900

    v0.1.6.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ extras_require = {"test": ["tox"]}
 
 setup(
     name="azurecost",
-    version="0.1.5",
+    version="0.1.6",
     description="Simple and easy command line to view azure costs.",
     long_description=open(os.path.join(here, "README.md")).read(),
     long_description_content_type="text/markdown",
